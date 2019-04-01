126294bed85ec7589b35edefe5f2a5c01809034b
ktvrdinic <karlotvrdinic@gmail.com>
2019-04-01 21:19:48 +0200

    Edit smart contract

--- contract.sol
+++ contract.sol
@@ -2,39 +2,44 @@ pragma solidity ^0.5.1;
 
 contract TripleCoin {
 
-    // Struktura soba 
     struct Soba{
         bool izvucena;
+        // Broj izvucenih osoba
         uint256 counter;
-        address adresaZaIsplatu;
-        address[] adrese;
+        // Stanje racuna po pojedinim sobama
+        uint256 balances;
+        address payable adresaZaIsplatu;
+        address payable []adrese;
     }
 
-    // Preslikavanje sobe s indeksom
+    // Tri sobe za kladenje
     mapping(uint256 => Soba) public soba_adrese;
     
-    // Ovdje se sprema random broj
+    // random broj
     uint public number;
 
-    // Postavljanje vrijednosti kroz konstruktor
     constructor() public {
         soba_adrese[0].izvucena = soba_adrese[1].izvucena = soba_adrese[2].izvucena = false;
         soba_adrese[0].counter = soba_adrese[1].counter = soba_adrese[2].counter = 0;
     }
 
-    // Metoda koja biljezi sve uplate
-    function setBet(uint256 soba, address a) public returns(bool){
+    // metoda koja biljezi sve uplate, te vrsi uplatu na ugovor
+    function setBet(uint256 soba) payable public returns(bool){
+        require((soba_adrese[soba].balances < ((soba*1 ether)+2 ether)) && (soba_adrese[soba].counter < (soba+2)) && (soba < 3));
         if(soba == 0 && soba_adrese[0].counter < 2){
-            soba_adrese[0].adrese.push(a);
+            soba_adrese[0].adrese.push(msg.sender);
             soba_adrese[0].counter++;
+            soba_adrese[soba].balances += msg.value;
             return true;
         }else if(soba == 1 && soba_adrese[1].counter < 3){
-            soba_adrese[1].adrese.push(a);
+            soba_adrese[1].adrese.push(msg.sender);
             soba_adrese[1].counter++;
+            soba_adrese[soba].balances += msg.value;
             return true;
         }else if(soba == 2 && soba_adrese[2].counter < 4){
-            soba_adrese[2].adrese.push(a);
+            soba_adrese[2].adrese.push(msg.sender);
             soba_adrese[2].counter++;
+            soba_adrese[soba].balances += msg.value;
             return true;
         }else{
             tryYourLuck(soba);
@@ -42,54 +47,62 @@ contract TripleCoin {
         }
     }
 
-    // Brise sve podatke i priprema za novu igru
+    // brise sve podatke nakon sto krug zavrsi
     function clearBets(uint256 soba) public {
+        require(soba < 3);
         if(soba == 0 && soba_adrese[0].counter == 2){
             delete soba_adrese[0].adrese;
             delete soba_adrese[0].adresaZaIsplatu;
             soba_adrese[0].counter=0;
             soba_adrese[0].izvucena = false;
+            soba_adrese[0].balances -= soba_adrese[0].balances;
         }else if(soba == 1 && soba_adrese[1].counter == 3){
             delete soba_adrese[1].adrese;
             delete soba_adrese[1].adresaZaIsplatu;
             soba_adrese[1].counter=0;
             soba_adrese[1].izvucena = false;
+            soba_adrese[1].balances -= soba_adrese[1].balances;
         }else if(soba == 2 && soba_adrese[2].counter == 4){
             delete soba_adrese[2].adrese;
             delete soba_adrese[2].adresaZaIsplatu;
             soba_adrese[2].counter=0;
             soba_adrese[2].izvucena = false;
+            soba_adrese[2].balances -= soba_adrese[2].balances;
         }
     }
 
-    // Metoda koja pronalazi pobjednike u određenoj sobi
+    // metoda koja pronalazi pobjednike
     function tryYourLuck(uint256 soba) public returns(uint256){
         if(soba == 0 && soba_adrese[0].counter == 2 && soba_adrese[0].izvucena == false){
             generateRandomNumber();
             soba_adrese[0].adresaZaIsplatu = soba_adrese[0].adrese[getRandomNumber()%2];
             soba_adrese[0].izvucena = true;
+            return getRandomNumber()%2;
         }else if(soba == 1 && soba_adrese[1].counter == 3 && soba_adrese[1].izvucena == false){
             generateRandomNumber();
             soba_adrese[1].adresaZaIsplatu = soba_adrese[1].adrese[getRandomNumber()%3];
             soba_adrese[1].izvucena = true;
+            return getRandomNumber()%3;
         }else if(soba == 2 && soba_adrese[2].counter == 4 && soba_adrese[2].izvucena == false){
             generateRandomNumber();
             soba_adrese[2].adresaZaIsplatu = soba_adrese[2].adrese[getRandomNumber()%4];
             soba_adrese[2].izvucena = true;
+            return getRandomNumber()%4;
         }
+        return 10;
     }
 
-    // Metoda koja dohvaca trenutni random broj
+    // metoda koja dohvaca trenutni random broj
     function getRandomNumber() public view returns(uint256){
         return number;
     }
     
-    // Metoda koja dohvaca trenutni random broj
+    // metoda koja dohvaca trenutni random broj
     function getIzvucenaSoba(uint256 soba) public view returns(bool){
         return soba_adrese[soba].izvucena;
     }
 
-    // Metoda vraća pobjednika
+    // Izbrisati
     function returnWinner(uint256 soba) public view returns(address){
         return soba_adrese[soba].adresaZaIsplatu;
     }
@@ -97,5 +110,20 @@ contract TripleCoin {
     // metoda za generiranje random broja
     function generateRandomNumber() public {
         number = (uint(keccak256(abi.encodePacked(block.number-1)))%10+1)%4;
+    }
+    
+    // Prebacivanje novaca na drugi racun
+    function transfer(uint256 soba) public {
+        require(soba_adrese[soba].balances == ((soba*1 ether)+2 ether));
+        if(soba_adrese[0].counter == 2 && soba == 0 && soba_adrese[0].izvucena == true){
+            soba_adrese[0].adresaZaIsplatu.transfer(2 ether);
+            soba_adrese[soba].balances -= 2 ether;
+        }else if(soba_adrese[1].counter == 3 && soba == 1 && soba_adrese[1].izvucena == true){
+            soba_adrese[1].adresaZaIsplatu.transfer(3 ether);
+            soba_adrese[soba].balances -= 3 ether;
+        }else if(soba_adrese[2].counter == 4 && soba == 2 && soba_adrese[2].izvucena == true){
+            soba_adrese[2].adresaZaIsplatu.transfer(4 ether);
+            soba_adrese[soba].balances -= 4 ether;
+        }
     }
 }
--- index.js
+++ index.js
@@ -4,77 +4,6 @@ var Racuni = new Array();
 // ABI ugovora
 var contractAbi = [
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "soba",
-				"type": "uint256"
-			}
-		],
-		"name": "clearBets",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "generateRandomNumber",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "soba",
-				"type": "uint256"
-			},
-			{
-				"name": "a",
-				"type": "address"
-			}
-		],
-		"name": "setBet",
-		"outputs": [
-			{
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "soba",
-				"type": "uint256"
-			}
-		],
-		"name": "tryYourLuck",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
 		"constant": true,
 		"inputs": [
 			{
@@ -94,31 +23,17 @@ var contractAbi = [
 		"type": "function"
 	},
 	{
-		"constant": true,
-		"inputs": [],
-		"name": "getRandomNumber",
-		"outputs": [
+		"constant": false,
+		"inputs": [
 			{
-				"name": "",
+				"name": "soba",
 				"type": "uint256"
 			}
 		],
+		"name": "transfer",
+		"outputs": [],
 		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "number",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
+		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
@@ -159,6 +74,10 @@ var contractAbi = [
 				"type": "uint256"
 			},
 			{
+				"name": "balances",
+				"type": "uint256"
+			},
+			{
 				"name": "adresaZaIsplatu",
 				"type": "address"
 			}
@@ -166,6 +85,101 @@ var contractAbi = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "generateRandomNumber",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "number",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getRandomNumber",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "soba",
+				"type": "uint256"
+			}
+		],
+		"name": "clearBets",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "soba",
+				"type": "uint256"
+			}
+		],
+		"name": "setBet",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "soba",
+				"type": "uint256"
+			}
+		],
+		"name": "tryYourLuck",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
 	}
 ];
 
@@ -179,24 +193,11 @@ function init() {
 		Racuni[i] = new Array();
 	}
 
-	// var adresa = $("#msgRPC").val();
-
 	var provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
-	window.web3 = new Web3(provider);	
+	window.web3 = new Web3(provider);
 
-	var $out = $("#racuni");
+	accounts = web3.eth.accounts;	
 
-	accounts = web3.eth.accounts;
-	Acc = accounts.shift();
-
-	provjeraRacuni = accounts;
-
-	var ispis = "";
-	for(var i=0;i<accounts.length;++i){
-		ispis += "<tr><td>"+ (i+1) + ".</td> <td>" + accounts[i] + "</td></tr>";
-	}
-	
-	$out.html(ispis);
 	window.spojeno = true;
 }
 
@@ -204,83 +205,49 @@ function isEmpty(str){
 	return !str.replace(/\s+/, '').length;
 }
 
-// metoda kao parametar metode transaction() - opis transakcije
-function callbackFn(err, info) {
-    console.log("Prošlo je!");
-    console.log("Greška: ", err);
-    console.log("Informacije: ", info);
+function changeSelect(){
+	var okladjeneAdrese = document.getElementById("okladjeniRacuni");
+	var soba = document.getElementById("msgRoom").value;
 
-    var $out = $("#out");
+	okladjeneAdrese.innerHTML = "<p>ROOM "+ (+soba+1) +" - Addresses that are in the bets:</p>";
 
-    if (!err) {
-		console.log("Transakcija poslana. TxID:\n" + info);
-    }
-    else {
-		console.log("Transakcija neuspješna. Razlog:\n" + err);
-    }
+	for (var i = 0; i < Racuni[+soba].length; ++i) {
+		okladjeneAdrese.innerHTML += (i+1) + ". Player: <i>" + Racuni[+soba][i] + "</i><br />";
+	}
 }
 
-function oklada(){	
-	var brojac = 0;
-
-	var adresa = document.getElementById("msgAddress").value; 
-	// var RPC = document.getElementById("-").value;
+function oklada(){
 	var soba = document.getElementById("msgRoom").value;
 	var okladjeneAdrese = document.getElementById("okladjeniRacuni");
 	var pobjednikText = document.getElementById("pobjednik");
 
 	var contr = web3.eth.contract(contractAbi).at(contractAddress);
 
-	if(isEmpty(adresa)){
-		alert("You need to fill address field.");
-	}else if(Racuni[+soba].length == (+soba + 2)){
+	if(Racuni[+soba].length == (+soba + 2)){
 		alert("All bets are made.");
-	}else if(!provjeraRacuni.includes(adresa.toLowerCase())){
-		alert("User account doesn't exist! Use another account.");
 	}else{
-		Racuni[+soba].push(adresa);
-		if(contr.setBet(+soba, adresa.toLowerCase(), {gas:1000000})){
-			var weiAmount = web3.toWei(1, 'ether');
-
-			var info = {
-				from: adresa,
-				to: Acc,
-				value: weiAmount
-			}
-			web3.eth.sendTransaction(info);
-		}
+		Racuni[+soba].push(web3.eth.defaultAccount);
+		contr.setBet(+soba, { from: web3.eth.defaultAccount, value: web3.toWei(1,'ether'), gas: 1000000}, function (err, result) {console.log(err);});
 	}
 
-	okladjeneAdrese.innerHTML = "<p>ROOM:"+ (+soba+1) +"! Addresses that are in the bets:</p>";
+	okladjeneAdrese.innerHTML = "<p>ROOM "+ (+soba+1) +" - Addresses that are in the bets:</p>";
 
 	for (var i = 0; i < Racuni[+soba].length; ++i) {
-		okladjeneAdrese.innerHTML += (i+1) + ". " + Racuni[+soba][i] + "<br />";
+		okladjeneAdrese.innerHTML += (i+1) + ". Player: <i>" + Racuni[+soba][i] + "</i><br />";
 	}
 	
 	if((Racuni[+soba].length == +soba + 2)){
-		var randomNumber = contr.tryYourLuck(+soba, {gas:1000000});	
+		var randomNumber = contr.tryYourLuck(+soba, {gas: 1000000});	
 		if(contr.getIzvucenaSoba(+soba)){
 			var adresaPobjednik = contr.returnWinner(+soba); 
 			
-			pobjednikText.innerHTML += "<p id='soba"+ soba +"'>Room number " + (+soba+1) + ": The winner is the player with the address " + adresaPobjednik +". Congratulations, the player won " + (+soba+2) +" ETH!</p>";
+			pobjednikText.innerHTML += "<p id='soba"+ soba +"'>Room number " + (+soba+1) + ": The winner is the player with the address <b>" + adresaPobjednik +"</b>. Congratulations, the player won " + (+soba+2) +" ETH!</p>";
 
-			var weiAmount = web3.toWei(+soba+2, 'ether');
-
-			var info = {
-				from: Acc,
-				to: adresaPobjednik,
-				value: weiAmount
-			}
-
-			web3.eth.sendTransaction(info, callbackFn);
+			contr.transfer(+soba, {to: adresaPobjednik, value: web3.toWei(+soba+2,'ether')}, {gas: 1000000});
 
 			contr.clearBets(+soba, {gas:1000000});
 		}
-
 	}
-	//var $out = $("#racuni");
-
-	//$out.html(ispis);
 }
 
 function clearRoom(){
@@ -289,11 +256,27 @@ function clearRoom(){
 	var okladjeneAdrese = document.getElementById("okladjeniRacuni");
 	var pobjednikText = document.getElementById("soba"+soba);
 
-	Racuni[soba].length = 0;
-	contr.clearBets(soba, {gas:100000});
-	okladjeneAdrese.innerHTML = "";	
+	Racuni[+soba].length = 0;
+	contr.clearBets(+soba, {gas:100000});
+	okladjeneAdrese.innerHTML = "<p>ROOM "+ (+soba+1) +" - Addresses that are in the bets:</p>";	
 	if(typeof(pobjednikText) != 'undefined' && pobjednikText != null)
 	{
 		pobjednikText.remove();
+	}
+}
+
+// metoda kao parametar metode transaction() - opis transakcije
+function callbackFn(err, info) {
+	console.log("Prošlo je!");
+	console.log("Greška: ", err);
+	console.log("Informacije: ", info);
+
+	var $out = $("#out");
+
+	if (!err) {
+	console.log("Transakcija poslana. TxID:\n" + info);
+	}
+	else {
+	console.log("Transakcija neuspješna. Razlog:\n" + err);
 	}
 }
